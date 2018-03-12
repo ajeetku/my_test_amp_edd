2f0f6f2445a5bf222972a6458983fe3826f2e1a2
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-03-12 11:24:46 +0530

    #14 merge test

--- amp-testing.php
+++ amp-testing.php
@@ -14,6 +14,7 @@ if (!defined('AMP_TESTING_VERSION')) {
 	define('AMP_TESTING_VERSION','0.0.1');
 }
 
+echo "sdcsdc";die;
 // this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
 define( 'AMP_TESTING_STORE_URL', 'https://wordpress-amp.000webhostapp.com' ); // you should use your own CONSTANT name, and be sure to replace it throughout this file
 
